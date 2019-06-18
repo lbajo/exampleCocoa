b2ba7faeafce98a56a7796b72005c6e0519b77a1
Lorena Bajo Rebollo <lbajo9@gmail.com>
2019-06-18 15:39:16 +0200

    update

---
+++ exampleCocoa/example.swift
@@ -0,0 +1,20 @@
+//
+//  example.swift
+//  exampleCocoa
+//
+//  Created by Lorena Bajo Rebollo on 18/6/19.
+//  Copyright © 2019 Lorena Bajo Rebollo. All rights reserved.
+//
+
+import Foundation
+
+public class example {
+    
+    init(){
+        print("example")
+    }
+    
+    func hello(){
+        print("hello")
+    }
+}
---
+++ exampleCocoa/exampleCocoa.h
@@ -0,0 +1,19 @@
+//
+//  exampleCocoa.h
+//  exampleCocoa
+//
+//  Created by Lorena Bajo Rebollo on 18/6/19.
+//  Copyright © 2019 Lorena Bajo Rebollo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for exampleCocoa.
+FOUNDATION_EXPORT double exampleCocoaVersionNumber;
+
+//! Project version string for exampleCocoa.
+FOUNDATION_EXPORT const unsigned char exampleCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <exampleCocoa/PublicHeader.h>
+
+
